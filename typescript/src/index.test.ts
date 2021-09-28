dcd216fbbdf22720439ca293df176c9283c98510
Benjamin Babik <ben@curlyben.com>
2021-09-28 23:33:57 +0000

    first test passing

--- typescript/src/index.test.ts
+++ typescript/src/index.test.ts
@@ -1,14 +1,20 @@
-beforeAll(async () => {
+import sqlite, { Database } from "sql.js";
+let db: Database;
 
+beforeAll(async () => {
+    const sql = await sqlite();
+    db = new sql.Database();
 })
 
 afterAll(async () => {
+    db.close();
+})
 
+test("db is okay", async () => {
+    const result = db.exec("SELECT 1;")
+    expect(result).toStrictEqual([{ "columns": ["1"], "values": [[1]] }]);
 })
 
 test("schema is okay", async () => {
-    const sqlite = await (await import("sql.js")).default();
-    const db = new sqlite.Database();
-    const result = db.exec("SELECT 1;")
-    expect(result).toBe(1);
+    
 })
