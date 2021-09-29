172eefd8f1877fbd82b45bbcc15442ad8b16fd7f
Benjamin Babik <ben@curlyben.com>
2021-09-29 15:09:08 +0000

    new schema and json search with object query

--- typescript/src/index.test.ts
+++ typescript/src/index.test.ts
@@ -1,5 +1,5 @@
 type Unwrap<P> = P extends Promise<infer T> ? Unwrap<T> : P;
-import type { createDb } from "./index"
+import { createDb, whereClauseToSql } from "./index"
 import sqlite, { Database } from "sql.js";
 let db: Database;
 
@@ -33,39 +33,109 @@ describe("sql", () => {
     test("insert-node & search-node-by-id & delete-node", async () => {
         await db.insertNode({ id: "1" });
 
-        const result = db.searchNodeById("1");
+        const result = db.getNodeById("1");
         expect(result).toEqual({ id: "1" });
 
         db.deleteNode("1");
-        const deleted = db.searchNodeById("1");
+        const deleted = db.getNodeById("1");
         expect(deleted).toBeUndefined();
     })
 
     test("insert-edge & search-edges & search-edge-by-id & delete-edge", async () => {
         db.insertNode({ id: "1" });
         db.insertNode({ id: "2" });
-        db.insertEdge("1", "2", { id: "12abc", fact: "abc" });
-        const result1 = db.searchNodeById("1");
+        db.insertEdge({ source: "1", target: "2", id: "12abc", fact: "abc" });
+        const result1 = db.getNodeById("1");
         expect(result1).toEqual({ id: "1" })
-        const result2 = db.searchNodeById("2");
+        const result2 = db.getNodeById("2");
         expect(result2).toEqual({ id: "2" })
 
         // from-to
-        const edgeFT = db.searchEdges("1", "2");
-        expect([...edgeFT]).toEqual([{ source: "1", target: "2", id: "12abc", properties: { id: "12abc", fact: "abc" } }]);
+        const edgeFT = db.getEdges("1", "2");
+        expect([...edgeFT]).toEqual([{ source: "1", target: "2", id: "12abc", fact: "abc" }]);
 
         // to-from
-        const edgeTF = db.searchEdges("2", "1", "toFrom");
-        expect([...edgeTF]).toEqual([{ source: "1", target: "2", id: "12abc", properties: { id: "12abc", fact: "abc" } }]);
+        const edgeTF = db.getEdges("2", "1", "toFrom");
+        expect([...edgeTF]).toEqual([{ source: "1", target: "2", id: "12abc", fact: "abc" }]);
 
         // both
-        const edgeXX = db.searchEdges("2", "1", "both");
-        expect([...edgeXX]).toEqual([{ source: "1", target: "2", id: "12abc", properties: { id: "12abc", fact: "abc" } }]);
+        const edgeXX = db.getEdges("2", "1", "both");
+        expect([...edgeXX]).toEqual([{ source: "1", target: "2", id: "12abc", fact: "abc" }]);
 
-        const edge = db.searchEdgeById("12abc")!
+        const edge = db.getEdgeById("12abc")!
         db.deleteEdge(edge.id);
-        const noEdge = db.searchEdgeById(edge.id);
+        const noEdge = db.getEdgeById(edge.id);
         expect(noEdge).toBeUndefined();
+    })
+
+    test("searchNodes", () => {
+        type nodeType = {
+            id: string
+            field: string
+        }
+        db.insertNode<nodeType>({ id: "a", field: "value" })
+        const nodes = [...db.searchNodes<nodeType>({ field: { eq: "value" } })]
+    })
+
+    test("insert-node & update-node", async () => {
+        db.insertNode({
+            id: "a",
+            tag: "0"
+        })
+        expect(db.getNodeById("a")).toEqual({ id: "a", tag: "0" })
+        db.updateNode({
+            id: "a",
+            tag: "1"
+        })
+        expect(db.getNodeById("a")).toEqual({ id: "a", tag: "1" })
+    })
+
+    test("whereClauseToSql", () => {
+        type fooBar = {
+            foo: string,
+            bar: string,
+        }
+        const explicitAndClause = whereClauseToSql<fooBar>({
+            foo: { eq: "bar" },
+            AND: { bar: { eq: "foo" } }
+        })
+        expect(explicitAndClause.sql).toBe("foo = :0 AND (bar = :1)")
+
+        const implicitAndClause = whereClauseToSql<fooBar>({
+            foo: { eq: "bar" },
+            bar: { eq: "foo" }
+        })
+        expect(implicitAndClause.sql).toBe("foo = :0 AND bar = :1")
+
+        const explicitOrClause = whereClauseToSql<fooBar>({
+            foo: { eq: "bar" },
+            OR: { bar: { eq: "foo" } }
+        })
+        expect(explicitOrClause.sql).toBe("foo = :0 OR (bar = :1)")
+    })
+
+    test("whereClauseToSql jsonField=root", () => {
+        type fooBar = {
+            foo: string,
+            bar: string,
+        }
+        const explicitAndClause = whereClauseToSql<fooBar>({
+            foo: { eq: "bar" },
+            AND: { bar: { eq: "foo" } }
+        }, "root")
+        expect(explicitAndClause.sql).toBe("json_extract(root, '$.foo') = :0 AND (json_extract(root, '$.bar') = :1)")
+
+        const implicitAndClause = whereClauseToSql<fooBar>({
+            foo: { eq: "bar" },
+            bar: { eq: "foo" }
+        }, "root")
+        expect(implicitAndClause.sql).toBe("json_extract(root, '$.foo') = :0 AND json_extract(root, '$.bar') = :1")
+
+        const explicitOrClause = whereClauseToSql<fooBar>({
+            foo: { eq: "bar" },
+            OR: { bar: { eq: "foo" } }
+        }, "root")
+        expect(explicitOrClause.sql).toBe("json_extract(root, '$.foo') = :0 OR (json_extract(root, '$.bar') = :1)")
     })
 
     afterEach(() => {
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import init, { Database } from "sql.js";
+import init, { BindParams, Database, SqlValue } from "sql.js";
 const sqlite = init();
 
 const loadSql = (filename: string) => {
@@ -13,10 +13,13 @@ export const createDb = async (schemaFile = "schema-new.sql") => {
         insertNode: <T extends { id: string }>(node: T) => {
             database.run(loadSql("insert-node.sql"), [JSON.stringify(node)]);
         },
+        updateNode: <T extends { id: string }>(node: T) => {
+            database.run(loadSql("update-node.sql"), [JSON.stringify(node), node.id])
+        },
         deleteNode: (id: string) => {
             database.run(loadSql("delete-node.sql"), [id]);
         },
-        searchNodeById: <T extends { id: string }>(id: string) => {
+        getNodeById: <T extends { id: string }>(id: string) => {
             const stmt = database.prepare(loadSql("search-node-by-id.sql"), [id]);
             if (stmt.step()) {
                 return JSON.parse(stmt.getAsObject().body!.toString()) as T;
@@ -24,37 +27,37 @@ export const createDb = async (schemaFile = "schema-new.sql") => {
                 return undefined;
             }
         },
-        insertEdge: <T extends { id: string }>(fromId: string, toId: string, properties: T = <T>{ id: `${fromId}:${toId}` }) => {
-            database.run(loadSql("insert-edge.sql"), [fromId, toId, JSON.stringify(properties)]);
+        insertEdge: <T extends { source: string, target: string, id?: string }>(edge: T) => {
+            database.run("INSERT INTO edges VALUES(json(?))", [JSON.stringify({ id: `${edge.source}:${edge.target}`, ...edge })]);
         },
         deleteEdge: (id: string) => {
             database.run("DELETE FROM edges WHERE id = ?", [id])
         },
-        searchEdgeById: <T extends { id: string }>(id: string): { id: string, source: string, target: string, properties: T } | undefined => {
+        getEdgeById: <T extends { id: string, source: string, target: string }>(id: string): T | undefined => {
             const stmt = database.prepare("SELECT * FROM edges wHERE id = ?", [id]);
             if (stmt.step()) {
-                return stmt.getAsObject() as any as { id: string, source: string, target: string, properties: T & { id: string } }
+                return JSON.parse(stmt.getAsObject().properties!.toString()) as T;
             } else {
                 return undefined;
             }
         },
-        searchEdges: function* <T = any>(fromId: string, toId: string, direction: "fromTo" | "toFrom" | "both" = "fromTo"): IterableIterator<{
-            id: string,
-            source: string,
-            target: string,
-            properties: T
-        }> {
+        getEdges: function* <T extends { id: string, source: string, target: string }>(fromId: string, toId: string, direction: "fromTo" | "toFrom" | "both" = "fromTo"): IterableIterator<T> {
             if (direction === "both") {
-                yield* this.searchEdges(toId, fromId);
+                yield* this.getEdges(toId, fromId);
                 direction = "fromTo";
             }
             const stmt = database.prepare(loadSql("search-edges.sql"), direction === "fromTo" ? [fromId, toId] : [toId, fromId]);
             while (stmt.step()) {
                 const edge = stmt.getAsObject() as any as { id: string, source: string, target: string, properties: string };
-                yield {
-                    ...edge,
-                    properties: JSON.parse(edge.properties) as (T & { id: string })
-                };
+                yield JSON.parse(edge.properties) as T;
+            }
+        },
+        // TODO: query builder
+        searchNodes: function* <T>(query: WhereClause<Partial<T>>) {
+            const where = whereClauseToSql(query, "body");
+            const stmt = database.prepare(`SELECT * FROM nodes WHERE ${where.sql}`, where.params)
+            while (stmt.step()) {
+                yield JSON.parse(stmt.getAsObject().body!.toString()) as T;
             }
         },
         raw: (sql: string) => {
@@ -64,4 +67,61 @@ export const createDb = async (schemaFile = "schema-new.sql") => {
             database.close();
         }
     }
+}
+
+export type WhereClauseOperation<FieldType> =
+    FieldType extends string ?
+    ({
+        eq: FieldType
+    } | {
+        like: FieldType
+    } | {
+        startsWith: FieldType
+    } | {
+        endsWith: FieldType
+    }) : {
+        eq: FieldType
+    }
+
+export type WhereClause<T> = {
+    [K in keyof T]: WhereClauseOperation<T[K]>
+} & {
+    AND?: WhereClause<T>
+} & {
+    OR?: WhereClause<T>
+}
+
+export const whereClauseToSql = <T>(query: WhereClause<Partial<T>>, jsonField?: string): {
+    sql: string,
+    params: SqlValue[]
+} => {
+    const result = Object.entries(query).reduce(({ sql, params }, [name, field], i) => {
+        if (["AND", "OR"].includes(name)) {
+            const intermediate = whereClauseToSql(field, jsonField);
+            return {
+                sql: `${sql} ${name} (${intermediate.sql.replace(/\d+/g, ss => (parseInt(ss) + params.length).toString())})`,
+                params: [...params, ...intermediate.params]
+            }
+        }
+        const stringOperators: Record<string, (value: string, index: number) => string> = {
+            eq: (value, index) => `= :${index}`,
+            like: (value, index) => `like '%${index}%'`,
+            startsWith: (value, index) => `like '${index}%'`,
+            endsWith: (value, index) => `like '%${index}'`,
+        }
+        const operators = {
+            "string": stringOperators
+        }
+        const [op, rhs] = Object.entries(field)[0]
+        return {
+            sql: `${sql}${/:\d+$/.test(sql) ? " AND" : ""} ${jsonField ? `json_extract(${jsonField}, '$.${name}')` : name} ${operators["string"][op](op, params.length)}`,
+            params: [
+                ...params, rhs
+            ]
+        }
+    }, { sql: "", params: [] as any[] })
+    return {
+        sql: result.sql.trim(),
+        params: result.params
+    };
 }
