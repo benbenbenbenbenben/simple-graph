0a53c44330e92d1039a3883e60096b80eb0969bb
Ben Babik <work@benjaminbabik.com>
2021-10-08 00:16:11 +0100

    lovely type

--- src/design/index2.ts
+++ src/design/index2.ts
@@ -1,5 +1,3 @@
-import { StringUnitLength } from "luxon"
-
 type PropType = number | string | boolean | { [x: string]: PropType }
 type Props = {
     [x: string]: PropType | PropType[]
@@ -15,9 +13,9 @@ type KnownTargets = {
 }
 
 type EdgeModel<
-    Name extends string,
-    Namespace extends string,
-    KnownProps extends Props | undefined = undefined,
+    Name extends string = string,
+    Namespace extends string = string,
+    KnownProps extends Props | undefined = any,
     > = {
         $type: "edge"
         $name: Name
@@ -25,10 +23,41 @@ type EdgeModel<
         $props: Extendable<KnownProps extends undefined ? (Props | undefined) : KnownProps>
     }
 
+type EdgeDescriptor<
+    N extends string = string,
+    NS extends string = string,
+    P extends Extendable<Props> = any,
+    SD extends VertexDescriptor = any,
+    TD extends VertexDescriptor = any> = {
+        $id: string
+        $type: "edge",
+        $name: N,
+        $ns: NS,
+        $props: P,
+        $source: SD
+        $target: TD
+    }
+
+
+type VertexModel<
+    Name extends string = string,
+    Namespace extends string = string,
+    KnownProps extends Props | undefined = any,
+    > = {
+        $id: string
+        $type: "vertex"
+        $name: Name
+        $ns: Namespace
+        $props: Extendable<KnownProps extends undefined ? (Props | undefined) : KnownProps>
+    }
+
+
+
 type VertexDescriptor<
     N extends string = string,
     NS extends string = string,
     P extends Extendable<Props> = any> = {
+        $id: string
         $type: "vertex",
         $name: N,
         $ns: NS,
@@ -43,16 +72,18 @@ const vertex = <N extends string = "", NS extends string = "">(
     return {
         as: <P extends Props | undefined = undefined>(defaultProps?: P) => {
             type Creator = P extends undefined ? <C extends Props>(props?: Extendable<C>) => VertexModel<N, NS, Extendable<C>> : <C extends P>(props: Extendable<C>) => VertexModel<N, NS, Extendable<C>>
-            type Descriptor = VertexDescriptor<N, NS, P extends undefined ? Props : P>
+            type Descriptor = VertexDescriptor<N, NS, Extendable<(P extends undefined ? Props : P)>>
             return {
                 describe: {
+                    $id: "$automatic" as const,
                     $type: "vertex" as const,
                     $name: name,
                     $ns: ns || "" as NS,
-                    $props: (defaultProps || {}) as Extendable<P>,
+                    $props: (defaultProps || {}) as Extendable<(P extends undefined ? Props : P)>,
                 } as Descriptor,
                 new: ((props: P) => {
                     return {
+                        $id: "$automatic",
                         $type: "vertex" as const,
                         $name: name,
                         $ns: ns || "" as NS,
@@ -73,24 +104,27 @@ const edge = <N extends string = "", NS extends string = "">(
                 to: <TD extends VertexDescriptor>(target: { describe: TD } | TD) => {
                     return {
                         as: <P extends Props | undefined = undefined>(defaultProps?: P) => {
-                            type ThisEdge = P extends undefined ? (props?: Extendable<Props>) => EdgeModel<N, NS, Extendable<Props>> : (props: Extendable<P>) => EdgeModel<N, NS, Extendable<P>>
+                            type Creator = P extends undefined ? <C extends Props>(props?: Extendable<C>) => EdgeModel<N, NS, Extendable<C>> : <C extends P>(props: Extendable<C>) => EdgeModel<N, NS, Extendable<C>>
+                            type Descriptor = EdgeDescriptor<N, NS, Extendable<(P extends undefined ? Props : P)>, SD, TD>
                             return {
                                 describe: {
+                                    $id: "$automatic" as const,
                                     $type: "edge" as const,
                                     $name: name,
                                     $ns: ns || "" as NS,
-                                    $props: defaultProps || {} as Extendable<P>,
+                                    $props: (defaultProps || {}) as Extendable<(P extends undefined ? Props : P)>,
                                     $source: "describe" in source ? source.describe : source,
                                     $target: "describe" in target ? target.describe : target,
-                                },
+                                } as Descriptor,
                                 new: ((props: P) => {
                                     return {
+                                        $id: "$automatic",
                                         $type: "edge" as const,
                                         $name: name,
                                         $ns: ns || "" as NS,
                                         $props: props || defaultProps || {} as Extendable<P>,
                                     }
-                                }) as ThisEdge
+                                }) as Creator
                             }
                         }
                     }
@@ -107,24 +141,25 @@ const company = vertex("company").as()
 const worksFor = edge("worksFor").from(person).to(company).as()
 
 const bob = person.new({ name: "Bob" })
-const acme = company.new({ name: "Acme"})
+const acme = company.new({ name: "Acme" })
 
 //insert(bob, [worksFor, acme])
 
-const insert = <V extends VertexModel, E, VT>(single: V, double: [e: E, t: VT]) => {
+const insert = <
+    V extends VertexModel,
+    E extends EdgeModel | { describe: EdgeDescriptor },
+    VT extends VertexModel>(
+        single: V, double?: [e: E, t: VT]
+    ) => {
+    single // ?
+    !double || double[0] // ?
+    !double || double[1] // ?
 
 }
 
-type VertexModel<
-    Name extends string = "",
-    Namespace extends string = "",
-    KnownProps extends Props | undefined = undefined,
-    > = {
-        $type: "vertex"
-        $name: Name
-        $ns: Namespace
-        $props: Extendable<KnownProps extends undefined ? (Props | undefined) : KnownProps>
-    }
+insert(bob) // ?
+insert(bob, [worksFor.new(), acme]) // ?
+insert(acme, [worksFor, bob]) // ?
 
 
 // const insert = <A>(a: A) => {
